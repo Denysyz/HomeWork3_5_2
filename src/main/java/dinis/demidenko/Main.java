c53bdefb9be42e0540b9a2136aa087642a22a1b8
dende <kawoonn@gmail.com>
2016-11-11 13:12:37 +0200

    finish. Version 1.00

--- src/main/java/dinis/demidenko/Main.java
+++ src/main/java/dinis/demidenko/Main.java
@@ -19,13 +19,97 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //addClient();
-        addGoods();
-        viem(2);
+        System.out.println("Add client - 1");
+        System.out.println("Add goods - 2");
+        System.out.println("Create order - 3");
+        System.out.println("Show clients - 4");
+        System.out.println("Show goods - 5");
+        System.out.println("Show orders - 6");
+        System.out.println("Exit - 0");
+       while (true){
+           Scanner sc = new Scanner(System.in);
+           int p = sc.nextInt();
+           switch (p){
+               case 1:
+                   addClient();
+                   break;
+               case 2:
+                   addGoods();
+                   break;
+               case 3:
+                   addOrder();
+                   break;
+               case 4:
+                   viem(1);
+                   break;
+               case 5:
+                   viem(2);
+                   break;
+               case 6:
+                   showOrders();
+                   break;
+               case 0:
+                   return;
+               default:
+                   System.out.println("Error comand");
+                   break;
+           }
+       }
+    }
+    private static void showOrders(){
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM order2");
+            PreparedStatement psCl = con.prepareStatement("SELECT * FROM clients");
+            PreparedStatement psGo = con.prepareStatement("SELECT * FROM goods");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
 
+                ResultSet rsCl = psCl.executeQuery();
+                while (rsCl.next()){
+                    if(rs.getInt(1) == rsCl.getInt(1)){
+                        System.out.print(rsCl.getString(4) + " " + rsCl.getString(3) + " - ");
 
+                    }
+                }
+                ResultSet rsGo = psGo.executeQuery();
+                while (rsGo.next()){
+                    if(rs.getInt(2) == rsGo.getInt(1)){
+                        System.out.println(rsGo.getString(2) + " " + rsGo.getString(4));
+                    }
+                }
 
-
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void addOrder(){
+        int tempCl = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+        System.out.println("Enter goods");
+        String goods = sc.nextLine();
+        try {
+            Statement st = con.createStatement();
+            PreparedStatement psCl = con.prepareStatement("SELECT * FROM clients");
+            PreparedStatement psGo = con.prepareStatement("SELECT * FROM goods");
+            ResultSet rsCl = psCl.executeQuery();
+            ResultSet rsGo = psGo.executeQuery();
+            while (rsCl.next()) {
+                if(rsCl.getString(4).equals(email)){
+                    tempCl = rsCl.getInt(1);
+                }
+            }
+            while (rsGo.next()){
+                if(rsGo.getString(2).equals(goods)){
+                    System.out.println(rsGo.getString(1));
+                    st.execute("INSERT INTO order2 VALUES(" + tempCl + ", " + rsGo.getInt(1) + ");");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     private static void addGoods(){
         Scanner sc = new Scanner(System.in);
